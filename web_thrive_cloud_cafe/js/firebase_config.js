211da2979c088925df1c9b4bd05ec61cea43e25f
araustincs@gmail.com <araustin@vt.edu>
2021-04-01 02:43:48 -0400

    Moved contents into web_thrive_cloud_cafe directory

---
+++ web_thrive_cloud_cafe/js/firebase_config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDn9grxDoSaDeRvNLs0suXtoGDuguKMnVE",
+    authDomain: "dsc-hunger.firebaseapp.com",
+    projectId: "dsc-hunger",
+    storageBucket: "dsc-hunger.appspot.com",
+    messagingSenderId: "660484933383",
+    appId: "1:660484933383:web:91dca1f6e3bc504f2c67a2",
+    measurementId: "G-6V2J1PVDLL"
+};
